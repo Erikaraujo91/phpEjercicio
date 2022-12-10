844dfa1c80d120de75d1e9628ba7aaa68e37cf47
Erikaraujo91 <119891717+Erikaraujo91@users.noreply.github.com>
2022-12-10 08:27:29 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,34 @@
+<footer class="page-footer grey darken-4">
+          <div class="container">
+            <div class="row">
+              <div class="col l8 m6 s12">
+                <h5 class="white-text">Contáctanos</h5>
+                <p class="grey-text text-lighten-4">
+                &#128205; C. Real, 11, 28860 Paracuellos de Jarama, Madrid<br>
+                &#128222; Telefono: 644973950 <br>
+                &#128236; E-mail: piscaandina22@gmail.com</a></p>
+              </div>
+              <div class="col l2 m4 offset-l2 s12">
+                <h5 class="white-text">Enlaces</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="index.php">Inicio</a></li>
+                  <li><a class="grey-text text-lighten-3" href="noticias.php">Noticias</a></li>
+                  <li><a class="grey-text text-lighten-3" href="registro.php">Registro</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container center">
+            © 2022 Copyright MasterD
+                        <a href="https://www.facebook.com/profile.php?id=100083407342925" target="_blank"><img src="assets/images/fb.jpg" alt="logo-facebook" id="facebook"></a>            
+            <a href="https://www.instagram.com/pisca.andina/" target="_blank"><img src="assets/images/ig.jpg" alt="logo-instagram" id="instagram"></a>
+            </div>
+          </div>
+        </footer>
+
+      <!-- Llamada al archivo javaScript -->
+      <script src="http://localhost/trabajoFinalPHP/js/script.js" ></script>
+      <!-- Llamada al archivo/cdn Materialize -->
+      <!-- <script src="/script/materialize.min.js"></script> -->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
