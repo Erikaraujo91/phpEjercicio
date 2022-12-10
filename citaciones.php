7332bad3757d4b11e3ba4e21a39f4d512b706d92
Erikaraujo91 <119891717+Erikaraujo91@users.noreply.github.com>
2022-12-10 08:30:11 +0100

    Update citaciones.php

--- citaciones.php
+++ citaciones.php
@@ -1,20 +1,9 @@
 <!DOCTYPE html>
   <html>
-    <head>
-      <meta charset="UTF-8">
-      <meta name="description" content="Pagina de practicas masterD"/>
-      <meta name="robots" content="NOFOLLOW"/>
-      <meta name="Author" content="Erik"/>
-      <meta name="keywords" content="Pisca, Andes, Gastronomia, Tequeños, Pastelitos, Pasteles, Empanadas"/>
-      <meta name="revisit-after" content="2 days"/>
-      <meta name="category" content="practicas"/>
-      <title>Pisca Andina</title>
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!-- importamos materialize -->
-      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"/>
-      <link rel="stylesheet" type="text/css"  href="css/estilos.css">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    </head>
+    
+    <?php 
+        include("header.php");
+    ?>
     <body class="orange lighten-3">
         <header>
         <nav class="orange darken-4">
@@ -104,38 +93,8 @@
                       $mysql->close();
                       ?>
     <div class="col-xs-offset-3 col-xs-6 col-xs-offset-3"><a class="form-control btn btn-primary" href="crear-cita.php">Solicitar una cita</a></div>
-                <footer class="page-footer grey darken-4">
-          <div class="container">
-            <div class="row">
-              <div class="col l8 m6 s12">
-                <h5 class="white-text">Contáctanos</h5>
-                <p class="grey-text text-lighten-4">
-                &#128205; C. Real, 11, 28860 Paracuellos de Jarama, Madrid<br>
-                &#128222; Telefono: 644973950 <br>
-                &#128236; E-mail: piscaandina22@gmail.com</a></p>
-              </div>
-              <div class="col l2 m4 offset-l2 s12">
-                <h5 class="white-text">Enlaces</h5>
-                <ul>
-                  <li><a class="grey-text text-lighten-3" href="index.php">Inicio</a></li>
-                  <li><a class="grey-text text-lighten-3" href="noticias.php">Noticias</a></li>
-                  <li><a class="grey-text text-lighten-3" href="registro.php">Registro</a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div class="footer-copyright">
-            <div class="container center">
-            © 2022 Copyright MasterD
-                        <a href="https://www.facebook.com/profile.php?id=100083407342925" target="_blank"><img src="assets/images/fb.jpg" alt="logo-facebook" id="facebook"></a>            
-            <a href="https://www.instagram.com/pisca.andina/" target="_blank"><img src="assets/images/ig.jpg" alt="logo-instagram" id="instagram"></a>
-            </div>
-          </div>
-        </footer>
-      <!-- Llamada al archivo javaScript -->
-      <script src="http://localhost/trabajoFinalPHP/js/script.js" ></script>
-      <!-- Llamada al archivo/cdn Materialize -->
-      <!-- <script src="/script/materialize.min.js"></script> -->
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <?php 
+        include("footer.php");
+      ?>
     </body>
   </html>
