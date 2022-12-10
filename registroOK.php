a41e88df5da328a70d65bbf6dfd4f51b0cc8b430
Erikaraujo91 <119891717+Erikaraujo91@users.noreply.github.com>
2022-12-10 08:42:35 +0100

    Add files via upload

---
+++ registroOK.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+  <html>
+    
+    <?php 
+        include("header.php");
+    ?>
+
+<body class="orange lighten-3">
+        <header>
+            <nav class="orange darken-4">
+                <div class="nav-wrapper">
+                <a href="index.php" class="brand-logo">Pisca Andina</a>
+                <a href="index.php" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+                <ul class="right hide-on-med-and-down">
+                    <li><a href="index.php">Inicio</a></li>
+                    <li><a href="noticias.php">Noticias</a></li>
+                    <li><a href="registro.php">Registro</a></li>
+                    <li class="active"><a href="login.php">Login</a></li><i class="Small material-icons">person</i>
+                </ul>
+                </div>
+            </nav>
+            <ul class="sidenav" id="mobile-demo">
+                <li><a href="index.php">Inicio</a></li>
+                <li><a href="noticias.php">Noticias</a></li>
+                <li><a href="registro.php">Registro</a></li>
+                <li class="active"><a href="login.php">Login</a></li><i class="Small material-icons">person</i>
+            </ul>
+        </header>
+                <div class="row container">
+                    <div class="col s12 center about">
+                        <h1>Accede a tu cuenta</h1>
+                    </div>
+                </div>
+
+<!-- Empieza formulario login -->
+    <div class="row container">
+            <div class="col s12 center about">
+              <div class="row center">
+              
+                registro correcto
+                <meta http-equiv="refresh" content="3;URL=http://localhost/trabajoFinalPHP/index.php"/>
+              </div>
+            </div>     	
+          </div>
+<!-- FIN formulario login -->
+          
+      <?php 
+        include("footer.php");
+      ?>
+
+    </body>
+  </html>
