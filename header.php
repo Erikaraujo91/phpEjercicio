1321cb42cee81634b63de88bd528d65592fb009d
Erikaraujo91 <119891717+Erikaraujo91@users.noreply.github.com>
2022-12-10 08:28:13 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,18 @@
+<head>
+      <meta charset="UTF-8">
+      <meta name="description" content="Pagina de practicas masterD"/>
+      <meta name="robots" content="NOFOLLOW"/>
+      <meta name="Author" content="Erik"/>
+      <meta name="keywords" content="Pisca, Andes, Gastronomia, Tequeños, Pastelitos, Pasteles, Empanadas"/>
+      <meta name="revisit-after" content="2 days"/>
+      <meta name="category" content="practicas"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <title>Pisca Andina</title>
+      <!-- llamamos iconos de materialize -->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!-- llamamos materialize -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+      <!-- <link type="text/css" rel="stylesheet" href="css/materialize.min.css"/> -->
+      <link rel="stylesheet" href="./css/estilos.css">
+
+    </head>
